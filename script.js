e17f4be1d1b735ddc96189b0f54f7ca43cfeed61
zxx <zioalfinois4@gmail.com>
2025-05-08 09:55:10 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ function getResponse() {
   var output = document.getElementById("output");
   // Aturan-aturan sederhana berdasarkan input pengguna
   if (input.toLowerCase().indexOf("edukasi") !== -1) {
-    output.innerHTML = "1.game pembelajaran bahasa 2.game matematika 3.game kuis 4.game puzzle 5.dan game simulasi. ";
+    output.innerHTML = "1.Game Pembelajaran Bahasa  2.Game Matematika  3.Game Kuis  4.Game Puzzle  5.Game Simulasi. ";
   } else if (input.toLowerCase().indexOf("aksi") !== -1) {
-    output.innerHTML = "1.free fire 2.pugb mobile 3.genshin inpact 4.Dead Trigger2 5.Modern Combat 5: eSports FPS";
+    output.innerHTML = "1.Free Fire  2.Pugb Mobile  3.Genshin Inpact  4.Dead Trigger2  5.Modern Combat 5: ESports FPS";
   } else if (input.toLowerCase().indexOf("tekateki") !== -1) {
-    output.innerHTML ="1.Brain test Triky puzzle 2.Word Crossword Search 3.QuizzLand: Tanya dan Jawab .";
+    output.innerHTML ="1.Brain test Triky puzzle  2.Word Crossword Search  3.QuizzLand: Tanya dan Jawab .";
   } else if ( input.toLowerCase().indexOf("horror") !== -1) {
-    output.innerHTML = "1.True Fear: Forsaken Souls1 2.Emily Wants to Play 3.Horrorfield 4.Japanese Doll";
+    output.innerHTML = "1.True Fear: Forsaken Souls1  2.Emily Wants to Play  3.Horrorfield  4.Japanese Doll";
   } else if (input.toLowerCase().indexOf("mobil balap") !== -1) {
-    output.innerHTML = "1.Mario Kart Tour 2.Real Racing3 3.Need for Speed: No Limits 4.Asphalt 9 Legends.";
+    output.innerHTML = "1.Mario Kart Tour  2.Real Racing3  3.Need for Speed: No Limits  4.Asphalt 9 Legends.";
   } else if (input.toLowerCase().indexOf("buat anak") !== -1) {
-    output.innerHTML = "1.Toca Life World 2.Coloring & Learn,Baby Panda's Body Adventure 3.Puzzle Kids – Animals Shapes and Jigsaw Puzzle";
+    output.innerHTML = "1.Toca Life World  2.Coloring & Learn,Baby Panda's Body Adventure  3.Puzzle Kids – Animals Shapes and Jigsaw Puzzle";
   } else if (input.toLowerCase().indexOf("olahraga ") !== -1) {
-    output.innerHTML ="1.EA Sports FC Mobile 2.NBA Live Mobile Basketball 3.Real Boxing 2 4.Golf Clash - Golfing Simulator";
+    output.innerHTML ="1.EA Sports FC Mobile  2.NBA Live Mobile Basketball  3.Real Boxing 2  4.Golf Clash - Golfing Simulator";
   } else if ( input.toLowerCase().indexOf("petualang") !== -1) {
-    output.innerHTML = "1.Crashland 2.Doom and Destiny Advanced 3.Lara Croft GO 4.Minicraft Pocket Edition 5.Secret of Mana";
+    output.innerHTML = "1.Crashland  2.Doom and Destiny Advanced  3.Lara Croft GO  4.Minicraft Pocket Edition  5.Secret of Mana";
  } else {
     output.innerHTML = "Maaf, saya tidak mengerti apa yang Anda maksud.";
   }
